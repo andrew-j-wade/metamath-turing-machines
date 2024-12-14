3f5ca629b52603cbe04bfcea032f03fec5ea1857
CatsAreFluffy <catisfluffy@gmail.com>
2024-12-13 23:49:33 -0800

    BLC BMS: 738->726

--- check.sh
+++ check.sh
@@ -1,11 +1,11 @@
 echo -n "ZFC:       "
 python3 nqlaconic.py --print-tm zf2.nql | wc -l
 python3 nqlaconic.py --print-subs zf2.nql > output_zf2.txt
-python3 assemble_blc.py loader
+python3 assemble_blc.py loader 0 0
 echo -n "Loader:    "
 python3 nqlaconic.py --print-tm lam_loader.nql | wc -l
 python3 nqlaconic.py --print-subs lam_loader.nql > output_loader.txt
-python3 assemble_blc.py bms
+python3 assemble_blc.py bms 0 1
 echo -n "BLC BMS:   "
 python3 nqlaconic.py --print-tm lam_bms.nql | wc -l
 python3 nqlaconic.py --print-subs lam_bms.nql > output_bms.txt
